4c1655c8f2bc8be03343aa4a3f525ef8c7be77df
otavio <107579773+tokiovangePROG@users.noreply.github.com>
2022-10-19 17:08:43 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var canvas;
+var backgroundImage, bgImg, animal1_img, animal2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, animal1, animal2,bush, grass;
+var animals = [];
+var bushImage, grassImage;
+//BP
+function preload() {
+  backgroundImage = loadImage("background.jpg");
+  animal1_img = loadImage("animal1.png");
+  animal2_img = loadImage("animal2.png");
+  track = loadImage("track.png");
+  bushImage = loadImage("bush.png");
+ grassImage = loadImage("grass.png");
+}
+
+//BP
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+ 
+}
+
+//BP
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
